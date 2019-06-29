773f73d2a106f9f038a8d45adee9056a9b78723c
Saulo de sousa Joseph <saulos.joseph@gmail.com>
2019-06-29 16:18:46 -0300

    local server to test

---
+++ src/indexDev.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const routes = require('./routes');
+
+const port = 3000;
+const app = express();
+
+app.use(express.json());
+
+app.use('/', routes);
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+});
