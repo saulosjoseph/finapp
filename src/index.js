5a9a02e62923d584b4e88b657825761ff55c69fd
Saulo de sousa Joseph <saulos.joseph@gmail.com>
2019-06-29 13:37:10 -0300

    add express

--- src/index.js
+++ src/index.js
@@ -1,31 +1,15 @@
 const teste = require('./controller/teste');
-const http = require('http');
+const express = require('express');
 const port = 3000;
 
-http.createServer((req, res) => {
-    const url = req.url;
-    let body = [];
-    req.on('error', (err) => {
-        console.error(err);
-    }).on('data', (chunk) => {
-        body.push(chunk);
-    }).on('end', () => {
-        body = Buffer.concat(body);
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        if(url ==='/' && req.method === 'GET'){
-            const test = teste.teste(body);
-            res.write(JSON.stringify(test));
-            res.end();
-        } else if(url ==='/calculate'){
-            const test = teste.teste();
-            res.write(JSON.stringify(test));
-            res.end();
-        } else {
-            res.statusCode = 404;
-            res.end();
-        }
-    });
-}).listen(port, () => {
-    console.log(`Server running at port ${port}`);
-});
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(teste.teste());
+})
+
+app.get('/calculate', (req, res) => {
+    res.send(teste.teste());
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
