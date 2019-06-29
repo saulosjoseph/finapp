87f7b19cb2629ee5ce5f3de8b7fb7bb6535b60f3
Saulo de sousa Joseph <saulos.joseph@gmail.com>
2019-06-29 16:23:15 -0300

    improve local server init

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const routes = require('./routes');
 
+const port = process.env.PORT || 3000;
+
 const app = express();
 
 app.use(express.json());
 
 app.use('/', routes);
 
-app.listen(process.env.PORT);
+app.listen(port);
