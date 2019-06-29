41a274744be7402502b7db87b944dbc7960aeee7
Saulo de sousa Joseph <saulos.joseph@gmail.com>
2019-06-29 17:39:39 -0300

    fix router error

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const calculate = require('./controller/calculate');
 
-app.use(function(req, res, next) {
+router.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
