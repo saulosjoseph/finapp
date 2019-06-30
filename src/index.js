a59398cc2a020270c26857381f0036f2cbaffcb9
Saulo de sousa Joseph <saulos.joseph@gmail.com>
2019-06-30 11:01:05 -0300

    dabatase connection

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const routes = require('./routes');
+require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
 
-const port = process.env.PORT || 3000;
+const uri = process.env.URI
+const port = process.env.PORT || 3333;
 
-const app = express();
+const client = new MongoClient(uri, { useNewUrlParser: true });
+try {
+    client.connect(err => {
+        const collection = client.db("test").collection("devices");
+        if (err) return console.log(err)
+        const db = client.db('finapp')
+    
+        const app = express();  
+        app.use(express.json());  
+        app.use('/', routes); 
+        app.post('/test', (req, res) => {
+            db.collection('test').insertOne(req.body, (err, result) => {
+                if (err) return console.log(err)
 
-app.use(express.json());
+                console.log('data save in database');
+                res.send('Ok');
+            })
+        })
 
-app.use('/', routes);
-
-app.listen(port);
+        app.listen(port);
+    });
+} catch (error) {
+    console.log(error);
+}
