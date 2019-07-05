9a228f7ab0cf5b45948621c8ba83a836a3583d97
Saulo de sousa Joseph <saulos.joseph@gmail.com>
2019-07-05 17:11:29 -0300

    add Procfile

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const routes = require('./routes');
 const port = process.env.PORT || 3333;
 
 try {
     const app = express();  
-    app.use(cors());
     app.use(express.json());  
     app.use('/', routes);
     app.listen(port);
